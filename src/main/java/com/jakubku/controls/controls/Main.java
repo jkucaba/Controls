1bafb423065b74b3cbdb722fac43081725e35305
Jakub <jakub.kucaba@gmail.com>
2023-10-20 12:48:24 +0200

    Label Control

--- src/main/java/com/jakubku/controls/controls/Main.java
+++ src/main/java/com/jakubku/controls/controls/Main.java
@@ -2,14 +2,31 @@ package com.jakubku.controls.controls;
 
 import javafx.application.Application;
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
-
+        VBox root = new VBox();
+        root.setAlignment(Pos.CENTER);
+        Label label = new Label("Hello World!");
+        Rectangle rectangle = new Rectangle(20,20, Color.RED);
+        label.setGraphic(rectangle);
+        label.setUnderline(true);
+        label.setFont(Font.font("Arial", FontWeight.BOLD, 18));
+        label.setTextFill(Color.GREEN);
+        root.getChildren().add(label);
+        Scene scene = new Scene(root, 500, 450);
+        stage.setTitle("Label Controls Example");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
