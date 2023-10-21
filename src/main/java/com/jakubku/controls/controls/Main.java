a90c3835c264aa58347721563fa8ea4428025549
Jakub <jakub.kucaba@gmail.com>
2023-10-21 09:06:33 +0200

    More Complex TabPane Example

--- src/main/java/com/jakubku/controls/controls/Main.java
+++ src/main/java/com/jakubku/controls/controls/Main.java
@@ -18,6 +18,15 @@ public class Main extends Application {
         Tab one = new Tab("One");
         Tab two = new Tab("Two");
         pane.getTabs().addAll(one,two);
+        //two.setClosable(false);
+        VBox contentOne = new VBox();
+        contentOne.setAlignment(Pos.CENTER);
+        contentOne.getChildren().addAll(new Label("Content for Tab One"));
+        VBox contentTwo = new VBox();
+        contentTwo.setAlignment(Pos.CENTER);
+        contentTwo.getChildren().addAll(new Button("Content Two"));
+        one.setContent(contentOne);
+        two.setContent(contentTwo);
         root.setCenter(pane);
         Scene scene = new Scene(root, 500, 450);
         root.requestFocus();
