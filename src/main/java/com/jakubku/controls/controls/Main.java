b1eb42fc639ea54e7b6d4ef8addc94a840ca8165
Jakub <jakub.kucaba@gmail.com>
2023-10-21 10:26:26 +0200

    TaleView Example

--- src/main/java/com/jakubku/controls/controls/Main.java
+++ src/main/java/com/jakubku/controls/controls/Main.java
@@ -18,17 +18,13 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         VBox root = new VBox();
-        root.setAlignment(Pos.CENTER);
-        ListView<Task> taskListView = new ListView<>();
-        ObservableList<Task> task = FXCollections.observableArrayList(
-                new Task("Create Java Project"),
-                new Task("Create Bug Report"),
-                new Task("Commit changes to repo")
+        TableView<User> userTableView = new TableView<>(UserUtility.getUsersList());
+        userTableView.getColumns().addAll(
+                UserUtility.getFirstNameColumn(),
+                UserUtility.getLastNameColumn(),
+                UserUtility.getBirthDateColumn()
         );
-        //adding custom cell to list
-        taskListView.setCellFactory(taskListView1 -> new CustomTaskCell());
-        taskListView.getItems().addAll(task);
-        root.getChildren().addAll(taskListView);
+        root.getChildren().addAll(userTableView);
         Scene scene = new Scene(root, 500, 450);
         root.requestFocus();
         stage.setTitle("ListView Control Example");
---
+++ src/main/java/com/jakubku/controls/controls/UserUtility.java
@@ -0,0 +1,33 @@
+package com.jakubku.controls.controls;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.time.LocalDate;
+
+public class UserUtility {
+    public static ObservableList<User> getUsersList(){
+        return FXCollections.observableArrayList(
+                new User("John", "Johnson", LocalDate.of(1997, 10,1)),
+                new User("Ken", "Block", LocalDate.of(2002, 1,6)),
+                new User("Sara", "Barney", LocalDate.of(1987, 3,7))
+        );
+    }
+    public static TableColumn<User, String> getFirstNameColumn(){
+        TableColumn<User, String> fNameCol = new TableColumn<>("First Name");
+        fNameCol.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        return fNameCol;
+    }
+    public static TableColumn<User, String> getLastNameColumn(){
+        TableColumn<User, String> lNameCol = new TableColumn<>("Last Name");
+        lNameCol.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+        return lNameCol;
+    }
+    public static TableColumn<User, LocalDate> getBirthDateColumn(){
+        TableColumn<User, LocalDate> bDateCol = new TableColumn<>("Birth Date");
+        bDateCol.setCellValueFactory(new PropertyValueFactory<>("birthDate"));
+        return bDateCol;
+    }
+}
