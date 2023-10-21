90fff5570b86ee3ab2986f296016bd7817122f96
Jakub <jakub.kucaba@gmail.com>
2023-10-21 09:33:39 +0200

    More Advanced ListView Example

--- src/main/java/com/jakubku/controls/controls/Main.java
+++ src/main/java/com/jakubku/controls/controls/Main.java
@@ -5,6 +5,7 @@ import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -17,10 +18,21 @@ public class Main extends Application {
     public void start(Stage stage) {
         VBox root = new VBox();
         root.setAlignment(Pos.CENTER);
+        Label selected = new Label();
+        Button selectFirst = new Button("Select First");
         ListView<String> list = new ListView<>();
         ObservableList<String> names = FXCollections.observableArrayList("Patrick", "Sam", "Ken", "Sara");
+        list.getSelectionModel()
+                .selectedItemProperty()
+                        .addListener(
+                                (observableValue, olVal, newVal) ->
+                                        selected.setText(newVal)
+                                );
+        //we can select multiple things
+        //list.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         list.getItems().addAll(names);
-        root.getChildren().addAll(list);
+        selectFirst.setOnAction(actionEvent -> list.getSelectionModel().selectFirst());
+        root.getChildren().addAll(selected, list, selectFirst);
         Scene scene = new Scene(root, 500, 450);
         root.requestFocus();
         stage.setTitle("ListView Control Example");
