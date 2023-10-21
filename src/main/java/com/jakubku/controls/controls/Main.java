64e07d84c409567554f792e12ce1c8d86b2c64c7
Jakub <jakub.kucaba@gmail.com>
2023-10-21 09:00:21 +0200

    Basic TabPane Example

--- src/main/java/com/jakubku/controls/controls/Main.java
+++ src/main/java/com/jakubku/controls/controls/Main.java
@@ -14,25 +14,14 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         BorderPane root = new BorderPane();
-        MenuBar menu = new MenuBar();
-        Menu file = new Menu("File");
-        MenuItem save = new MenuItem("Save");
-        MenuItem exit = new MenuItem("Exit");
-        exit.setOnAction(actionEvent -> Platform.exit());
-        file.getItems().addAll(save, exit);
-        Menu edit = new Menu("Edit");
-        RadioMenuItem option = new RadioMenuItem("Options");
-        RadioMenuItem settings = new RadioMenuItem("Settings");
-        Menu prefs = new Menu("Preferences");
-        MenuItem prefOne = new MenuItem("Pref:One");
-        MenuItem prefTwo = new MenuItem("Pref:Two");
-        edit.getItems().addAll(option, settings, new SeparatorMenuItem(), prefs);
-        prefs.getItems().addAll(prefOne, prefTwo);
-        menu.getMenus().addAll(file, edit);
-        root.setTop(menu);
+        TabPane pane = new TabPane();
+        Tab one = new Tab("One");
+        Tab two = new Tab("Two");
+        pane.getTabs().addAll(one,two);
+        root.setCenter(pane);
         Scene scene = new Scene(root, 500, 450);
         root.requestFocus();
-        stage.setTitle("Menu Control Example");
+        stage.setTitle("TabPane Control Example");
         stage.setScene(scene);
         stage.show();
     }
