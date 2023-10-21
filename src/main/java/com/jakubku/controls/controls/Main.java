cfdc906e283e364f91102cf9985a0b1478c2ba6d
Jakub <jakub.kucaba@gmail.com>
2023-10-21 12:48:26 +0200

    Editing a TreeView

--- src/main/java/com/jakubku/controls/controls/Main.java
+++ src/main/java/com/jakubku/controls/controls/Main.java
@@ -10,7 +10,12 @@ import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.control.cell.TextFieldTreeCell;
+import javafx.scene.control.cell.TextFieldTreeTableCell;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -22,6 +27,7 @@ public class Main extends Application {
         TreeView<String> treeView = new TreeView<>();
         TreeItem<String> classes = new TreeItem<>("Classes");
         TreeItem<String> classOne = new TreeItem<>("Class One");
+        //classOne.setExpanded(true);
         classOne.getChildren().addAll(
                 new TreeItem<>("Patric"),
                 new TreeItem<>("Sam"),
@@ -29,13 +35,39 @@ public class Main extends Application {
                 new TreeItem<>("Sara")
         );
         TreeItem<String> classTwo = new TreeItem<>("Class Two");
+        //classTwo.setExpanded(true);
         classTwo.getChildren().addAll(
                 new TreeItem<>("Peter"),
                 new TreeItem<>("Abigail"),
                 new TreeItem<>("Rick")
         );
+        classTwo.addEventHandler(TreeItem.branchExpandedEvent(), e ->
+                System.out.println(e.getTreeItem().getValue()));
         classes.getChildren().addAll(classOne, classTwo);
         treeView.setRoot(classes);
+        treeView.setShowRoot(false); // We see classes and not root
+        treeView.setCellFactory(TextFieldTreeCell.forTreeView());
+        treeView.setEditable(true);
+        /*treeView.setCellFactory(tCell -> {
+            TreeCell<String> cell = new TreeCell<>(){
+                @Override
+                public void updateItem(String item, boolean empty){
+                    super.updateItem(item, empty);
+                    setText(null);
+                    setGraphic(null);
+                    if(!empty){
+                        if(this.getTreeItem().getValue() == "Patric"){
+                            setText(item);
+                            setGraphic(new Rectangle(15,15, Color.RED));
+                        } else {
+                            setText(item);
+                            setGraphic(new Rectangle(15,15, Color.VIOLET));
+                        }
+                    }
+                }
+            };
+            return cell;
+        });*/
         root.getChildren().addAll(treeView);
         Scene scene = new Scene(root, 500, 450);
         root.requestFocus();
