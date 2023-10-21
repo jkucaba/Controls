284af6ff6e67571ac3e01c949895244f33d882b5
Jakub <jakub.kucaba@gmail.com>
2023-10-21 09:20:57 +0200

    Basic ListView Example

--- src/main/java/com/jakubku/controls/controls/Main.java
+++ src/main/java/com/jakubku/controls/controls/Main.java
@@ -2,6 +2,8 @@ package com.jakubku.controls.controls;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -13,24 +15,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-        BorderPane root = new BorderPane();
-        TabPane pane = new TabPane();
-        Tab one = new Tab("One");
-        Tab two = new Tab("Two");
-        pane.getTabs().addAll(one,two);
-        //two.setClosable(false);
-        VBox contentOne = new VBox();
-        contentOne.setAlignment(Pos.CENTER);
-        contentOne.getChildren().addAll(new Label("Content for Tab One"));
-        VBox contentTwo = new VBox();
-        contentTwo.setAlignment(Pos.CENTER);
-        contentTwo.getChildren().addAll(new Button("Content Two"));
-        one.setContent(contentOne);
-        two.setContent(contentTwo);
-        root.setCenter(pane);
+        VBox root = new VBox();
+        root.setAlignment(Pos.CENTER);
+        ListView<String> list = new ListView<>();
+        ObservableList<String> names = FXCollections.observableArrayList("Patrick", "Sam", "Ken", "Sara");
+        list.getItems().addAll(names);
+        root.getChildren().addAll(list);
         Scene scene = new Scene(root, 500, 450);
         root.requestFocus();
-        stage.setTitle("TabPane Control Example");
+        stage.setTitle("ListView Control Example");
         stage.setScene(scene);
         stage.show();
     }
